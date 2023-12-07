83f964456133db0cbc049a3ac805d633a664deeb
Brandon <brandiicoetzee@gmail.com>
2023-12-07 18:58:58 +0200

    Refactor NightfallActivationListener class
    Removed unused import statements in the NightfallActivationListener.kt file.

--- src/main/kotlin/com/github/coeiico/nightfall/listeners/NightfallActivationListener.kt
+++ src/main/kotlin/com/github/coeiico/nightfall/listeners/NightfallActivationListener.kt
@@ -1,10 +1,6 @@
 package com.github.coeiico.nightfall.listeners
 
-import com.intellij.ide.customize.transferSettings.models.EditorColorScheme
 import com.intellij.openapi.application.ApplicationActivationListener
-import com.intellij.openapi.application.runInEdt
-import com.intellij.openapi.diagnostic.thisLogger
-import com.intellij.openapi.editor.colors.EditorColorsManager
 import com.intellij.openapi.wm.IdeFrame
 
 internal class NightfallActivationListener : ApplicationActivationListener {
